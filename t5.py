a053a7404dc3d62231f80f1d0ec760e4d4e396a9
eng-aliyan <85061004+eng-aliyan@users.noreply.github.com>
2021-07-01 17:06:34 +0430

    bug fixed

--- t5.py
+++ t5.py
@@ -1,3 +1,5 @@
+import math
+
 inNumbers = []
 for i in range(10):
     n = int(input())
@@ -5,10 +7,11 @@ for i in range(10):
 
 squares = []
 for i in inNumbers:
-    squares.append(i**2)
+    squares.append(math.pow(i, 2))
 
 sqNumbers = {}
 for i in range(10):
-    sqNumbers[inNumbers[i]] = squares[i]
+    num = inNumbers[i]
+    sqNumbers[num] = int(math.pow(num, 3))
 
 print(sqNumbers)
