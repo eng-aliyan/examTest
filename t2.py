fd648c5b5de89db82d0a2afbe2ce16372bf8a557
arya <eng.aliyan20@gmail.com>
2021-07-19 17:41:05 +0430

    just a test for git

--- t2.py
+++ t2.py
@@ -1,39 +1,23 @@
-passwd = input()
 
-uppers = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+passwd = input('[o]: ')
 
-lower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+a, b, c = 48, 65, 97
+cases = [["$", "#", "@"], range(a, a+11), range(b, b+27), range(c, c+27)]
 
-nums = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+isValid = True
+if not 6 <= len(passwd) <= 16:
+	isValid = False
 
-cases = ["$", "#", "@"]
-
-inRange = True
-if len(passwd) < 6 or len(passwd) > 16:
-    inRange = False
-
-isUpper = False
-for item in uppers:
-    if item in passwd:
-        isUpper = True
-        
-isLower = False
-for item in lower:
-    if item in passwd:
-        isLower = True
-        
-isNum = False
-for item in nums:
-    if item in passwd:
-        isNum = True
-        
-isCases = False
-for item in cases:
-    if item in passwd:
-        isCases = True
-
-isValid = inRange and isUpper and isLower and isNum and isCases
-
+if isValid:
+	passed = False
+	for item in cases:
+		for c in item:
+			if '%c'%c in passwd:
+				passed = True
+				break
+	if not passed:
+		isValid = False
+  
 if isValid:
     print("passwd is valid")
 else:
