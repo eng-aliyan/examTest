1b8663d54e446a51287b10f5149b1977bc26f0ca
eng-aliyan <85061004+eng-aliyan@users.noreply.github.com>
2021-07-19 17:47:53 +0430

    spaces removed =)

--- t2.py
+++ t2.py
@@ -1,25 +1,18 @@
-
 passwd = input('[o]: ')
-
-a, b, c = 48, 65, 97
-cases = [["$", "#", "@"], range(a, a+11), range(b, b+27), range(c, c+27)]
-
+cases = [["$", "#", "@"], range(48, 59), range(65, 92), range(97, 124)]
 isValid = True
 if not 6 <= len(passwd) <= 16:
 	isValid = False
-
 if isValid:
 	passed = False
 	for item in cases:
 		for c in item:
-			if '%c'%c in passwd:
+			if ('%c'%c) in passwd:
 				passed = True
 				break
 	if not passed:
 		isValid = False
-  
 if isValid:
     print("passwd is valid")
 else:
     print("passwd is invalid")
-
